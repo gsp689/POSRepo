f3c34adfcb918abe68fbd3c5823b220f08033c10
ganeshsp <ganesh.patil@objectedge.com>
2016-11-03 18:05:34 +0530

    new class

--- POS/src/com/javatpoint/ItemRetrieverHelper.java
+++ POS/src/com/javatpoint/ItemRetrieverHelper.java
@@ -1,5 +1,25 @@
 package com.javatpoint;
 
+import java.util.List;
+
+import com.pos.models.Order;
+import com.pos.models.Table;
+
 public class ItemRetrieverHelper {
 
+	private List<Order> order;
+	
+	public List<Order> getAllOrders(Table table){
+		
+		return order;
+	}
+	
+public String getTableData(Table table){
+		String tableData = null;
+		
+		
+		return tableData;
+	}
+	
+	
 }
